153da19ab33294780658bee4c3b4f97ff9cb1770
eons44 <eons@eons.dev>
2022-09-16 11:37:57 -0700

    update things!

--- scripts/mkwp.sh
+++ scripts/mkwp.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-site=$1
+org=$1
+if [ -z $org ]; then
+    echo "please supply the owning namespace"
+    exit 1
+fi
+
+site=$2
 if [ -z $site ]; then
     echo "please supply a site name"
     exit 1
 fi
 
-domain=$2
+domain=$3
+preagree=false
 if [ -z $domain ]; then
     domain="$site.eons.dev"
+else [ $domain == y ]
+  preagree=true
 fi
 
-namespace="wp-$site"
+namespace="$1"
 release="$site"
 #chart="cm/wordpress"
 chart="/home/eons/git/fog/charts/hostpath/wordpress"
 
-valuesPath="/home/eons/inf/k8s/helm/release/wp/${release}"
-valuesFile="${valuesPath}/values.yaml"
+valuesPath="/home/eons/inf/k8s/helm/release/wp/${org}"
+valuesFile="${valuesPath}/${release}.yaml"
 
 if [ ! -f $valuesFile ]; then
     mkdir -pv $valuesPath
@@ -54,23 +63,30 @@ echo "\n########################"
 existenceCheck=$(helm ls -A | grep $namespace | grep $release)
 echo $existenceCheck
 if [ -z "$existenceCheck" ]; then
-	echo "Could not find ${namespace}/${release}"
-	read -p "Create new site? [Y/n] " -n 1 -r
-	echo ""
-	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    	exit 0
-	fi
+  echo "Could not find ${namespace}/${release}"
+  echo "Will run:"
+  echo "helm -n $namespace install $release $chart -f $valuesFile"
+  if ! $preagree; then
+  	read -p "Create new site? [Y/n] " -n 1 -r
+  	echo ""
+  	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      	exit 0
+  	fi
+  fi
 	kubectl create namespace $namespace
-	#echo "helm -n $namespace install $release $chart -f $valuesFile"
 	helm -n $namespace install $release $chart -f $valuesFile
-	echo "Now hosting $domain in namesapce $namespace"
+	echo "Now hosting $site in namesapce $namespace"
 else
-	echo "${namespace}/${release} exists"
-	read -p "Upgrade? [Y/n] " -n 1 -r
-	echo ""
-	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    	exit 0
-	fi
+  echo "${namespace}/${release} exists"
+  echo "Will run:"
+  echo "helm -n $namespace upgrade $release $chart -f $valuesFile"
+  if ! $preagree; then
+  	read -p "Upgrade? [Y/n] " -n 1 -r
+  	echo ""
+  	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      	exit 0
+  	fi
+  fi
 	helm -n $namespace upgrade $release $chart -f $valuesFile
-	echo "Upgraded $domain"
+	echo "Upgraded $site"
 fi
